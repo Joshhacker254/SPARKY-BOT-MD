0dd7a0cf2a039088217d97f978090e854d4fd5e8
Joshhacker254 <96799252+Joshhacker254@users.noreply.github.com>
2023-04-05 02:36:23 +0300

    Update sparky.js

--- sparky.js
+++ sparky.js
@@ -40,9 +40,9 @@ const ini_mark = `0@s.whatsapp.net`
 const ownernya = ownernomer + '@s.whatsapp.net'
 
 //TIME
-const xtime = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-const xdate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
-const time2 = moment().tz('Asia/Kolkata').format('HH:mm:ss')  
+const xtime = moment.tz('Africa/Nairobi').format('HH:mm:ss')
+const xdate = moment.tz('Africa/Nairobi').format('DD/MM/YYYY')
+const time2 = moment().tz('Africa/Nairobi').format('HH:mm:ss')  
  if(time2 < "23:59:00"){
 var ucapanWaktu = 'Good night 🌌'
  }
